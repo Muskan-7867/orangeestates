6f6d8fc642f0e5e8de546ea331720b31cfe9c549
Muskan-7867 <muskanloach984@gmail.com>
2026-06-19 18:28:11 +0530

    make responsive

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,24 +1,22 @@
-
 export default function Features() {
     return (
-        <div className="flex justify-around mt-10 mb-10 px-36">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-y-8 gap-x-4 my-12 px-6 sm:px-16 md:px-24 lg:px-36">
             <div className="text-center">
-                <h1 className="text-3xl font-medium">1100</h1>
-                <p className="text-[#9B9393]">Offices Worldwide</p>
-            </div>
-            <div className="text-center">
-                <h1 className="text-3xl font-medium">25,000</h1>
-                <p className="text-[#9B9393]">Sales Associates</p>
+                <h1 className="text-2xl sm:text-3xl font-medium">1100</h1>
+                <p className="text-[#9B9393] text-sm sm:text-base">Offices Worldwide</p>
             </div>
             <div className="text-center">
-                <h1 className="text-3xl font-medium">200</h1>
-                <p className="text-[#9B9393]">Countries & Territories</p>
+                <h1 className="text-2xl sm:text-3xl font-medium">25,000</h1>
+                <p className="text-[#9B9393] text-sm sm:text-base">Sales Associates</p>
             </div>
-            <div className="text-center ">
-                <h1 className="text-3xl font-medium">85</h1>
-                <p className="text-[#9B9393]">Years in Business</p>
+            <div className="text-center">
+                <h1 className="text-2xl sm:text-3xl font-medium">200</h1>
+                <p className="text-[#9B9393] text-sm sm:text-base">Countries & Territories</p>
             </div>
-
+            <div className="text-center">
+                <h1 className="text-2xl sm:text-3xl font-medium">85</h1>
+                <p className="text-[#9B9393] text-sm sm:text-base">Years in Business</p>
+            </div>
         </div>
     )
 }
