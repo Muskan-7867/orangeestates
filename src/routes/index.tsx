fb19a3383bdce1f8c01ef47ef8616ae87de1a310
Muskan-7867 <muskanloach984@gmail.com>
2026-06-23 18:36:40 +0530

    add new files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,18 @@
-import Features from '#/components/Features'
-import HeroSection from '#/features/home/components/HeroSection'
+import Features from '#/features/home/components/Features'
+import HeroBannerCarousel from '#/features/home/components/HeroBannerCarousel'
 import Images from '#/features/home/components/Images'
-
 import Navbar from '#/components/Navbar'
-
-import { Footer } from '#/components/ui/Footer'
 import { createFileRoute } from '@tanstack/react-router'
 import Spotlight from '#/features/home/components/SpotLight'
 import ScrollImages from '#/features/home/components/ScrollImages'
 import PropertyGrid from '#/features/home/components/PropertyGrid'
 import WhyChooseUs from '#/features/home/components/WhyChooseUs'
-import Demo from '#/components/ui/demo'
 import CTA from '#/features/home/components/CTA'
+import ImageCarousel from '#/features/home/components/ImageCarousel'
+import { FaqsSection } from '#/components/faqs-1'
+import Testimonials from '#/features/home/components/Testimonials'
+import StickyFooter from '#/components/ui/Footer'
+
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
@@ -19,20 +20,21 @@ function Home() {
     <>
       <Navbar />
 
-      <HeroSection />
-
+      <HeroBannerCarousel />
+      {/* <HeroSection /> */}
       <Images />
       <Features />
-
-
       <ScrollImages />
       <Spotlight />
       <PropertyGrid />
       <WhyChooseUs />
-      <Demo />
+      <ImageCarousel />
+      <Testimonials />
+      <FaqsSection />
       <CTA />
-      {/* <Hero /> */}
-      <Footer />
+
+
+      <StickyFooter />
     </>
   )
 }
