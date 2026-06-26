4d3455f62a08b1d2353bfd91e3bf55d2d6bacbec
Muskan-7867 <muskanloach984@gmail.com>
2026-06-26 18:38:35 +0530

    update data

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,11 +1,13 @@
 export const properties = [
   {
+    id: "1",
     images: [
-      "https://images.unsplash.com/photo-1600585154526-990dced4db0d?w=1200&q=80",
+    
       "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1200&q=80",
       "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=1200&q=80",
-
-
+        "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1200&q=80",
+      "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=1200&q=80",
+      "https://sothebysrealty.co.uk/cdn-cgi/image/format=auto,width=600,height=500,fit=cover,quality=75/https://uk-media.s3.amazonaws.com/Listing_s3/UK-S-07678_img_4243hero.jpg.jpeg"
     ],
     title: "Peninsula Heights, Albert Embankment, London",
     beds: 4,
@@ -14,12 +16,11 @@ export const properties = [
     price: "£ 9,500,000",
   },
   {
+    id: "2",
     images: [
       "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1200&q=80",
       "https://images.unsplash.com/photo-1600585154526-990dced4db0d?w=1200&q=80",
-
       "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=1200&q=80",
-
     ],
     title: "Hanover Square, Mayfair, London",
     beds: 3,
@@ -28,10 +29,10 @@ export const properties = [
     price: "£ 7,500,000",
   },
   {
+    id: "3",
     images: [
       "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=1200&q=80",
       "https://images.unsplash.com/photo-1600585154526-990dced4db0d?w=1200&q=80",
-
       "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=1200&q=80",
     ],
     title: "Craven Hill Gardens, Bayswater, London",
@@ -40,40 +41,40 @@ export const properties = [
     area: "2,863 SQ.FT.",
     price: "£ 4,250,000",
   },
-   {
-    images:[
+  {
+    id: "4",
+    images: [
       "https://images.unsplash.com/photo-1600585154526-990dced4db0d?w=1200&q=80",
       "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1200&q=80",
       "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=1200&q=80",
-
     ],
-    title: "Peninsula Heights, Albert Embankment, London",
+    title: "Peninsula Heights, Albert Embankment, London (Duplicate)",
     beds: 4,
     baths: 4,
     area: "4,000 SQ.FT.",
     price: "£ 9,500,000",
   },
   {
-    images:[
+    id: "5",
+    images: [
       "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=1200&q=80",
       "https://images.unsplash.com/photo-1600585154526-990dced4db0d?w=1200&q=80",
       "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=1200&q=80",
-
     ],
-    title: "Hanover Square, Mayfair, London",
+    title: "Hanover Square, Mayfair, London (Duplicate)",
     beds: 3,
     baths: 4,
     area: "1,280 SQ.FT.",
     price: "£ 7,500,000",
   },
   {
-    images:[
+    id: "6",
+    images: [
       "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=1200&q=80",
       "https://images.unsplash.com/photo-1600585154526-990dced4db0d?w=1200&q=80",
-
       "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=1200&q=80",
     ],
-    title: "Craven Hill Gardens, Bayswater, London",
+    title: "Craven Hill Gardens, Bayswater, London (Duplicate)",
     beds: 4,
     baths: 3,
     area: "2,863 SQ.FT.",
