a910e855f13b533089fa7f685d6f1a793a759f2b
Muskan-7867 <muskanloach984@gmail.com>
2026-07-06 18:32:56 +0530

    update size

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -4,7 +4,7 @@ import { Link } from "@tanstack/react-router";
 export default function Logo({ className = "", src = "/estate.png", ...props }: { className?: string, src?: string, [key: string]: any }) {
     return (
         <Link to="/" className={cn(`flex   ${className}`, props)}>
-            <img fetchPriority="high" src={src} alt="logo" className="h-14 w-auto max-w-35 object-contain transition-all duration-300 ease-in-out" />
+            <img fetchPriority="high" src={src} alt="logo" className="h-10 sm:h-14 w-auto max-w-35 object-contain transition-all duration-300 ease-in-out" />
    
         </Link>
     )
