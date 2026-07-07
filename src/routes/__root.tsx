651ea2bb288daaef90953bbb750b0c5a834dfd6b
Muskan-7867 <muskanloach984@gmail.com>
2026-07-07 18:04:54 +0530

    update title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,10 +3,6 @@ import {
   Scripts,
   createRootRouteWithContext,
 } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import appCss from '../styles.css?url'
 
@@ -28,7 +24,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Orange Estates',
       },
     ],
     links: [
