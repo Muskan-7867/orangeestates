407dfe62067687fd3a9dc3f411d10d733e268d18
Muskan-7867 <muskanloach984@gmail.com>
2026-07-23 18:15:13 +0530

    auth pages

--- src/routes/auth/register.tsx
+++ src/routes/auth/register.tsx
@@ -1,4 +1,4 @@
-import { SignupForm } from '#/features/auth/components/signup-form'
+import { SignupForm } from '#/features/auth/components/SignupForm'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/auth/register')({
