cc939b7fa4887a9e28cfd080a5189e1f05c829e0
Muskan-7867 <muskanloach984@gmail.com>
2026-07-23 18:13:43 +0530

    move to another folders

--- src/routes/_user/properties.$slug.tsx
+++
@@ -1,6 +0,0 @@
-import PropertyDetailPage from '#/features/properties/components/PropertyDetailPage'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_user/properties/$slug')({
-  component: PropertyDetailPage,
-})
